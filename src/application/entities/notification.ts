4af9ef7910c6a7481e40499b46a7c05e7af5f7bb
DJehSantana <jessicah.dias23@hotmail.com>
2022-12-17 10:41:49 -0300

    criando getters e setters da classe Notification

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,8 +1,10 @@
 //define quais atributos uma notificação terá
 export interface NotificationProps {
+    recipientId: string;
     content: string;
     category: string;
     readAt?: Date | null;
+    createdAt: Date;
 }
 
 
@@ -14,6 +16,14 @@ export class Notification {
         this.props = props;
     }
 
+    public set recipientId(recipientId: string) {
+        this.props.recipientId = recipientId;
+    }
+
+    public get recipientId(): string {
+        return this.props.recipientId;
+    }
+
     public set content(content: string) {
         this.props.content = content;
     }
@@ -21,6 +31,29 @@ export class Notification {
     public get content(): string {
         return this.props.content;
     }
+
+
+    public set category(category: string) {
+        this.props.category = category;
+    }
+
+    public get category(): string {
+        return this.props.category;
+    }
+
+
+    public set readAt(readAt: Date | null | undefined) {
+        this.props.readAt = readAt;
+    }
+
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
+
+    public get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
 }
 
 
