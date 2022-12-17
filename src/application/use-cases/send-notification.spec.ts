4a59e74514dd0da987bdb9c51321c9f50fd3fa70
DJehSantana <jessicah.dias23@hotmail.com>
2022-12-17 13:00:49 -0300

    criando teste usando repositorio com armazenamento de dados em memória

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,28 +1,21 @@
-import { Notification } from "../entities/notification";
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
 import { SendNotification } from "./send-notification"
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-    async create(notification: Notification) {
-        notifications.push(notification);
-    }
-};
 
 describe('Send notification', () => {
     test('Notificação habilitada para o envio', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
 
         // o execute chama o método create
-        await sendNotification.execute({
+        const { notification } = await sendNotification.execute({
             recipientId: 'example-recipient-id',
             content: 'Uma nova notificação',
             category: 'social'
         });
 
-        console.log(notifications);
-
         //é esperado que o array tenha pelo menos um item
-        expect(notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications).toHaveLength(1);
+        //o objeto recebido por parametro deve ser o mesmo do criado no array
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
     });
 });
